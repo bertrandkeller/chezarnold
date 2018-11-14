b6cb3ced2336c9627d116dfdcd73c2574bd2b745
Bertrand Keller <bertrand.keller@gmail.com>
2018-11-14 22:30:36 +0100

    Optimize images

--- sw.js
+++ sw.js
@@ -27,15 +27,13 @@ const offlinePages = [
 function updateStaticCache() {
   return caches.open(staticCacheName)
     .then(cache => {
-      // These items won't block the installation of the Service Worker
       cache.addAll([
-        //'/assets/js/instantclick.min.js',
+        '/assets/js/instantclick.min.js',
       ].concat(offlinePages));
-      // These items must be cached for the Service Worker to complete installation
       return cache.addAll([
-        //'/assets/js/fetch-inject.js',
+        '/assets/js/fetch-inject.js',
         '{{ "/assets/css/main.css" | relative_url }}',
-        //'{{ "/assets/css/font.css" | relative_url }}',
+        '{{ "/assets/css/font.css" | relative_url }}'
       ]);
     });
 }
@@ -45,7 +43,6 @@ function stashInCache(cacheName, request, response) {
     .then(cache => cache.put(request, response));
 }
 
-// Limit the number of items in a specified cache.
 function trimCache(cacheName, maxItems) {
   caches.open(cacheName)
     .then(cache => {
@@ -59,7 +56,6 @@ function trimCache(cacheName, maxItems) {
     });
 }
 
-// Remove caches whose name is no longer valid
 function clearOldCaches() {
   return caches.keys()
     .then(keys => {
@@ -93,19 +89,15 @@ self.addEventListener('fetch', event => {
   let request = event.request;
   let url = new URL(request.url);
 
-  //Ignore non-GET requests
   if (request.method !== 'GET') {
     return;
   }
-  
-  // For HTML requests, try the network first, fall back to the cache, finally the offline page
+
   if (request.headers.get('Accept').includes('text/html')) {
-    
+
     event.respondWith(
       fetch(request)
       .then(response => {
-        // NETWORK
-        // Stash a copy of this page in the pages cache
         let copy = response.clone();
         if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
           stashInCache(staticCacheName, request, copy);
@@ -115,7 +107,6 @@ self.addEventListener('fetch', event => {
         return response;
       })
       .catch(() => {
-        // CACHE or FALLBACK
         return caches.match(request)
           .then(response => response || caches.match('/offline/'));
       })
@@ -123,15 +114,12 @@ self.addEventListener('fetch', event => {
     return;
   }
 
-  // For non-HTML requests, look in the cache first, fall back to the network
   event.respondWith(
     caches.match(request)
     .then(response => {
       // CACHE
       return response || fetch(request)
         .then(response => {
-          // NETWORK
-          // If the request is for an image, stash a copy of this image in the images cache
           if (request.headers.get('Accept').includes('image')) {
             let copy = response.clone();
             stashInCache(imagesCacheName, request, copy);
@@ -139,8 +127,6 @@ self.addEventListener('fetch', event => {
           return response;
         })
         .catch(() => {
-          // OFFLINE
-          // If the request is for an image, show an offline placeholder
           if (request.headers.get('Accept').includes('image')) {
             return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {
               headers: {
